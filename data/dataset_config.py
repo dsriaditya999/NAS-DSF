7967bbaa87682ae929d57792a74c6695d706fc2b
Sri Aditya Deevi <dsriaditya999@gmail.com>
2023-05-02 15:05:09 -0700

    git ignore corrected

--- data/dataset_config.py
+++ data/dataset_config.py
@@ -15,25 +15,25 @@ from effdet.data.dataset_config import *
 #     ))
 
 
-# @dataclass
-# class FlirAlignedCfg(CocoCfg):
-#     variant: str = ''
-#     splits: Dict[str, dict] = field(default_factory=lambda: dict(
-#         train=dict(ann_filename='images_thermal_train_day/day_flir_train.json', img_dir='images_thermal_train_day/data/', has_labels=True),
-#         val=dict(ann_filename='images_thermal_train_day/day_flir_val.json', img_dir='images_thermal_train_day/data/', has_labels=True),
-#         test=dict(ann_filename='images_thermal_test_day/day_flir.json', img_dir='images_thermal_test_day/data/', has_labels=True),
-#     ))
-
-
-
 @dataclass
 class FlirAlignedCfg(CocoCfg):
     variant: str = ''
     splits: Dict[str, dict] = field(default_factory=lambda: dict(
-        train=dict(ann_filename='images_thermal_train_night/night_flir_train.json', img_dir='images_thermal_train_night/data/', has_labels=True),
-        val=dict(ann_filename='images_thermal_train_night/night_flir_val.json', img_dir='images_thermal_train_night/data/', has_labels=True),
-        test=dict(ann_filename='images_thermal_test_night/night_flir.json', img_dir='images_thermal_test_night/data/', has_labels=True),
+        train=dict(ann_filename='images_thermal_train_day/day_flir_train.json', img_dir='images_thermal_train_day/data/', has_labels=True),
+        val=dict(ann_filename='images_thermal_train_day/day_flir_val.json', img_dir='images_thermal_train_day/data/', has_labels=True),
+        test=dict(ann_filename='images_thermal_test_day/day_flir.json', img_dir='images_thermal_test_day/data/', has_labels=True),
     ))
+
+
+
+# @dataclass
+# class FlirAlignedCfg(CocoCfg):
+#     variant: str = ''
+#     splits: Dict[str, dict] = field(default_factory=lambda: dict(
+#         train=dict(ann_filename='images_thermal_train_night/night_flir_train.json', img_dir='images_thermal_train_night/data/', has_labels=True),
+#         val=dict(ann_filename='images_thermal_train_night/night_flir_val.json', img_dir='images_thermal_train_night/data/', has_labels=True),
+#         test=dict(ann_filename='images_thermal_test_night/night_flir.json', img_dir='images_thermal_test_night/data/', has_labels=True),
+#     ))
 
 
 @dataclass
